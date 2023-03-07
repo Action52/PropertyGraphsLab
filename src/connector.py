1026d51f9a00f7411afe22a6bc8ac8c600e0ec51
Luis Alfredo Leon Villapun <alfredo.leon@10-192-15-86client.eduroam.upc.edu>
2023-03-07 12:50:27 +0100

    Exercise A.3 first upload. Still needs updates.

--- src/connector.py
+++ src/connector.py
@@ -5,7 +5,6 @@ References:
    - https://towardsdatascience.com/create-a-graph-database-in-neo4j-using-python-4172d40f89c4
 """
 
-
 from neo4j import GraphDatabase
 import logging
 
@@ -13,14 +12,16 @@ import logging
 class Neo4jConnector:
     def __init__(self, uri, user, password):
         self.driver = GraphDatabase.driver(uri, auth=(user, password))
-        logging.basicConfig("connector.log")
+        logging.basicConfig(filename="connector.log")
 
     def close(self):
         self.driver.close()
 
     def query(self, query):
+        session = None
+        response = None
         try:
-            session = self.driver.session(database=self.database)
+            session = self.driver.session()
             response = list(session.run(query))
         except Exception as e:
             logging.error(f"Query Failed: {e}")
@@ -28,3 +29,8 @@ class Neo4jConnector:
             if session is not None:
                 session.close()
             return response
+
+    def drop(self):
+        self.query(query="MATCH (n) -[e] -> () DELETE n, e")
+        self.query(query="MATCH (n) DELETE n")
+        logging.info("Deleted database. Success.")
